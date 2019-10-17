18472a2d7b4244b2265c0cf231bcc1561dc5322a
Cris Pham <ct96.pham@gmail.com>
2019-10-17 11:31:03 +0700

    fix fav

--- ng-sempiternal/src/app/profile/favorite.directive.ts
+++ ng-sempiternal/src/app/profile/favorite.directive.ts
@@ -4,21 +4,20 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appFavorite]'
 })
 export class FavoriteDirective {
-  @Input() favourite: boolean;
+  @Input() favourite: string;
   constructor(private eleRef: ElementRef) {
   }
   @HostListener('click') changeColor() {
-    if (this.favourite) {
-      console.log(this.eleRef);
+    if (this.favourite === 'true') {
       this.eleRef.nativeElement.classList.remove('favorite');
-      this.favourite = false;
-      // tslint:disable-next-line:max-line-length
-      this.eleRef.nativeElement.innerHTML = '<i _ngcontent-nyj-c4="" class="ion-heart"></i> ' + (parseInt(this.eleRef.nativeElement.innerText, 10) + 1);
-    } else if (!this.favourite) {
-      this.eleRef.nativeElement.classList.add('favorite');
-      this.favourite = true;
+      this.favourite = 'false';
       // tslint:disable-next-line:max-line-length
       this.eleRef.nativeElement.innerHTML = '<i _ngcontent-nyj-c4="" class="ion-heart"></i> ' + (parseInt(this.eleRef.nativeElement.innerText, 10) - 1);
+    } else if (this.favourite === 'false') {
+      this.eleRef.nativeElement.classList.add('favorite');
+      this.favourite = 'true';
+      // tslint:disable-next-line:max-line-length
+      this.eleRef.nativeElement.innerHTML = '<i _ngcontent-nyj-c4="" class="ion-heart"></i> ' + (parseInt(this.eleRef.nativeElement.innerText, 10) + 1);
     }
   }
 }
