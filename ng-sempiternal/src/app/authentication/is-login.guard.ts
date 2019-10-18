f7d6a04c937d6407efd86c2b6f2ee1995e809361
Cris Pham <ct96.pham@gmail.com>
2019-10-18 13:11:34 +0700

    fix guard

--- ng-sempiternal/src/app/authentication/is-login.guard.ts
+++ ng-sempiternal/src/app/authentication/is-login.guard.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsLoginGuard implements CanActivate {
+  constructor(private router: Router) {
+
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (localStorage.getItem('user')) {
       return true;
     }
+    this.router.navigate(['/signin']);
     return false;
   }
 
